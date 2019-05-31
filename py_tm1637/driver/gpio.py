3ca18443e13269c4f932e340e23c43f7a18bfc37
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2019-05-31 17:15:26 +0200

    Using simple implementation to load GPIO driver.
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- py_tm1637/driver/gpio.py
+++ py_tm1637/driver/gpio.py
@@ -1,43 +1,14 @@
 
-from collections import namedtuple
+GPIO_LIBS = ['gpiozero']
 
-Library = namedtuple('Library', 'instance name')
-
-GPIO_LIBS = ['gpiozero', 'rpi']
-
-
-def import_gpio_lib(libraries):
-    for gpio_lib in libraries:
-        try:
-            return Library(__import__(gpio_lib), gpio_lib)
-        except ImportError:
-            continue
-    return Library(None, 'None')
-
-
-module = import_gpio_lib(GPIO_LIBS)
-
-if module.name == 'gpiozero':
-
-    class Pin:
-        def __init__(self, pin_id, mode='in'):
-            self.pin = module.instance.OutputDevice(Converter[pin_id])
-
-        @property.setter
-        def state(self, value):
-            self.pin.value(value)
-
-
-if module.name == 'rpi':
-
-    class Pin:
-        pass
-
-
+for gpio_lib in GPIO_LIBS:
+    try:
+        instance = __import__('gpio_{}'.format(gpio_lib))
+        Pin = instance.Pin
+        converter = instance.converter
+        break
+    except ImportError:
+        continue
 else:
     raise ImportError(
-            'GPIO wrapper needs at least {} modules'.format(GPIO_LIBS))
-
-
-Converter = {
-    11: 17}
+        'GPIO wrapper needs at least {} modules'.format(GPIO_LIBS))
